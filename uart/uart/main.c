61af560491abd3d218022f04efc85ff937af0c64
cuki <mauriciocukier@gmail.com>
2017-04-08 11:38:29 -0300

    Signed-off-by: cuki <mauriciocukier@gmail.com>

--- uart/uart/main.c
+++ uart/uart/main.c
@@ -14,19 +14,20 @@
 #include <util/delay.h>
 
 #include "uart.h"
-
 #define UART_BAUD_RATE      9600
+
 
 int main(void)
 {
 	uint8_t data[3] = {1, 2, 3};
 	
 	DDRB |= _BV(DDB0);
-	uart_init();
+	uart_init( UART_BAUD_SELECT(UART_BAUD_RATE,F_CPU) );
 	sei();
 	
 	while (1)
 	{
+		uart_puts("Hello\n");
 		uart_send(data, 3);
 		PORTB ^= _BV(PORTB0);
 		_delay_ms(1000);
