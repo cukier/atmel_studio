fd81f3b9ef52b2e7308471855a6ab26c1b170685
cuki <mauriciocukier@gmail.com>
2017-04-10 14:18:42 -0300

    Signed-off-by: cuki <mauriciocukier@gmail.com>

--- Modbus/Modbus/i2c.h
+++ Modbus/Modbus/i2c.h
@@ -15,16 +15,16 @@
 #define SUCCESS		0
 #define ERROR		1
 
+void TWISetPageSize(uint16_t pSize);
+void TWISetWordAddress(void);
+void TWIResetWordAddress(void);
 void TWISetAddress(uint8_t address);
-void TWIInit(uint8_t address);
-void TWIStart(void);
-void TWIWrite(uint8_t u8data);
-uint8_t TWIReadACK(void);
-uint8_t TWIReadNACK(void);
-uint8_t TWIGetStatus(void);
-uint8_t EEWriteByte(uint16_t u16addr, uint8_t u8data);
-uint8_t EEReadByte(uint16_t u16addr, uint8_t *u8data);
-uint8_t EEWritePage(uint8_t page, uint8_t *u8data);
-uint8_t EEReadPage(uint8_t page, uint8_t *u8data);
+void TWIInit(void);
+uint8_t TWIWriteByte(uint16_t u16addr, uint8_t u8data);
+uint8_t TWIReadByte(uint16_t u16addr, uint8_t *u8data);
+uint8_t TWIReadData(uint16_t address, uint8_t *data, uint16_t size);
+uint8_t TWIWriteData(uint16_t address, uint8_t *data, uint16_t size);
+uint8_t TWIWritePage(uint8_t page, uint8_t *u8data);
+uint8_t TWIReadePage(uint8_t page, uint8_t *u8data);
 
 #endif /* I2C_H_ */
--- Modbus/Modbus/main.c
+++ Modbus/Modbus/main.c
@@ -1,21 +1,85 @@
 /*
-* Calendario.c
+* EEPROM.c
 *
-* Created: 04/04/2017 16:41:48
+* Created: 09/04/2017 20:49:16
 * Author : cuki
 */
 
 #include "sys.h"
+#include "i2c.h"
+#include "uart.h"
+#include <avr/io.h>
 #include <util/delay.h>
-#include <>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+
+#define TEST_SIZE	10
 
 int main(void)
-{	
-	DDRB |= (1 << DDB0);
+{
+	uint8_t data[TEST_SIZE] = {0};
+	uint8_t dataW[TEST_SIZE] = {0};
+	uint8_t dataR[TEST_SIZE] = {0};
+	char aux[5];
+	uint16_t cont;
+	uint8_t r;
+	
+	r = ERROR;
+	
+	for (cont = 0; cont < TEST_SIZE; ++cont)
+	{
+		dataW[cont] = (uint8_t) cont;
+	}
+	
+	TWIInit();
+	TWISetWordAddress();
+	TWISetAddress(0xA0);
+	uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
+	sei();
+	_delay_ms(300);
 	
 	while (1)
 	{
-		PORTB ^= (1 << PORTB0);
+		r = ERROR;
+		TWIReadData(0, data, TEST_SIZE);
+		r = TWIWriteData(0, dataW, TEST_SIZE);
+		
+		if (r == SUCCESS)
+		{
+			uart_puts("Escrita ok\n\r");
+		}
+		else
+		{
+			uart_puts("Escrita no ok\n\r");
+		}
+		
+		r = ERROR;
+		r = TWIReadData(0, dataR, TEST_SIZE);
+		
+		if (r == SUCCESS)
+		{
+			uart_puts("Leitura ok\n\r");
+		}
+		else
+		{
+			uart_puts("Leitura no ok\n\r");
+		}
+		
+		for (cont = 0; cont < TEST_SIZE; ++cont)
+		{
+			itoa(cont, aux, 16);
+			uart_puts(aux);
+			uart_puts(":");
+			itoa(data[cont], aux, 16);
+			uart_puts(aux);
+			uart_puts(" ");
+			itoa(dataR[cont], aux, 16);
+			uart_puts(aux);
+			uart_puts(" ");
+			itoa(dataW[cont], aux, 16);
+			uart_puts(aux);
+			uart_puts("\n\r");
+		}
 		_delay_ms(1000);
 	}
 	
