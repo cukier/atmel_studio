4f28a3f8373cac45bf9304536e835a348f752406
cuki2 <mauricio@krebs.com.br>
2017-04-12 13:10:39 -0300

    Signed-off-by: cuki2 <mauricio@krebs.com.br>

--- Datalogger/Datalogger/main.c
+++ Datalogger/Datalogger/main.c
@@ -1,25 +1,50 @@
 /*
- * Datalogger.c
- *
- * Created: 12/04/2017 11:58:53
- * Author : cuki
- */
+* Datalogger.c
+*
+* Created: 12/04/2017 11:58:53
+* Author : cuki
+*/
 
- #include "sys.h"
- #include "i2c.h"
- #include "uart.h"
- #include "eeprom.h"
- #include "ds1307.h"
- #include "modbus.h"
+#include "sys.h"
+#include "i2c.h"
+#include "uart.h"
+#include "eeprom.h"
+#include "ds1307.h"
+#include "modbus.h"
 
 #include <avr/io.h>
+#include <string.h>
+#include <util/delay.h>
 
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	uint8_t sec;
+	uint8_t n;
+	uint8_t arr[sizeof(DS1307_t)];
+	
+	sec = 0;
+	
+	uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
+	TWIInit();
+	eeprom_init();
+	
+	DS1307_t ds1307 = {0};
+	
+	while (1)
+	{
+		DS1307_get(&ds1307);
+		
+		if (sec != ds1307.seconds)
+		{
+			n = sizeof(DS1307_t);
+			memcpy(arr, &ds1307, n);			
+			eeprom_write_data(0, arr, n);
+			_delay_ms(300);
+		}
+		
+	}
+	
+	return -1;
 }
 
