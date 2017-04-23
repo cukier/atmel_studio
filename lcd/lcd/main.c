8b126563fe338a59746ff0b276eceed8e86c0948
cuki <mauriciocukier@gmail.com>
2017-04-22 21:04:49 -0300

    lcd
    Signed-off-by: cuki <mauriciocukier@gmail.com>

--- lcd/lcd/main.c
+++ lcd/lcd/main.c
@@ -1,9 +1,9 @@
 /*
- * lcd.c
- *
- * Created: 22/04/2017 12:27:43
- * Author : cuki
- */ 
+* lcd.c
+*
+* Created: 22/04/2017 12:27:43
+* Author : cuki
+*/
 
 #include "sys.h"
 #include "lcd.h"
@@ -16,17 +16,19 @@
 
 int main(void)
 {
-	//uint8_t cont;
+	lcd_init();
+	_delay_ms(100);
+	lcd_puts("\fhello");
+	lcd_puts("\nworld");
+	_delay_ms(1000);
 	
-	//cont = 0;
-    lcd_init(LCD_DISP_ON);
-	lcd_clrscr();
-	
-    while (1) 
-    {
-		lcd_puts("Hola viteh :)\n");
+	while (1)
+	{
+		lcd_puts("\fhello");
 		_delay_ms(1000);
-    }
+		lcd_puts("\nworld");
+		_delay_ms(1000);
+	}
 	
 	return 0;
 }
