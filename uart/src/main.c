47666dba5cd65b63add3850ee54b93399ac7ac8d
cuki2 <mauricio@krebs.com.br>
2017-10-02 16:31:56 -0300

    uart echo
    Signed-off-by: cuki2 <mauricio@krebs.com.br>

--- uart/src/main.c
+++ uart/src/main.c
@@ -2,15 +2,36 @@
 #include "uart.h"
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
 int main(void)
 {
-	uart1_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
-	_delay_ms(200);
-	uart1_puts("Hello\n\r");
+	uint16_t n;
 	
-	while(1);
+	n = 0;
+	uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
+	sei();
+	_delay_ms(200);
+	
+	while(1)
+	{
+		do 
+		{
+			n = uart_available();
+			_delay_ms(10);
+		} while (uart_available() != n);
+		
+		while(n--)
+		{
+			uart_putc(uart_getc());
+			
+			if (n == 0)
+			{
+				uart_flush();
+			}
+		}
+	}
 	
 	return 0;
 }
