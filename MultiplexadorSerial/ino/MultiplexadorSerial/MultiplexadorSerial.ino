f6b3c80e26c4c8de80c84da624b8106e41b460ab
cuki2 <mauricio@krebs.com.br>
2017-11-06 16:49:46 -0200

    Recebendo

--- MultiplexadorSerial/ino/MultiplexadorSerial/MultiplexadorSerial.ino
+++ MultiplexadorSerial/ino/MultiplexadorSerial/MultiplexadorSerial.ino
@@ -1,5 +1,26 @@
+#define NR_PORTAS 0x02
+
 int AD0 = 14;
 int AD1 = 15;
+int porta = 0;
+
+void set_serial(int serial) {
+  switch (serial) {
+    case 0:
+    default:
+      digitalWrite(AD0, LOW);
+      digitalWrite(AD1, LOW);
+      digitalWrite(LED_BUILTIN, HIGH);
+      break;
+    case 1:
+      digitalWrite(AD0, HIGH);
+      digitalWrite(AD1, LOW);
+      digitalWrite(LED_BUILTIN, LOW);
+      break;
+  }
+
+  delay(10);
+}
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -8,30 +29,55 @@ void setup() {
   Serial.begin(9600);
 }
 
-void set_serial(int serial) {
-  switch (serial) {
-    case 0:
-    default:
-      digitalWrite(AD0, LOW);
-      digitalWrite(AD1, LOW);
-      break;
-    case 1:
-      digitalWrite(AD0, HIGH);
-      digitalWrite(AD1, LOW);
-      break;
+void fun1(void) {
+  set_serial(0);
+  Serial.println("Ligado\n");
+  delay(1000);
+  set_serial(1);
+  Serial.println("Desligado\n");
+  delay(1000);
+}
+
+int m_listen(void) {
+  int ret = 0;
+
+  do {
+    ret = Serial.available();
+    delay(10);
+  } while (ret != Serial.available());
+
+  return ret;
+}
+
+void fun2(void) {
+  int n;
+  int i;
+
+  set_serial(porta);
+  n = m_listen();
+
+  if (n != 0) {
+    Serial.print("Recebindo na porta ");
+    Serial.print(porta);
+    Serial.print(" ");
+    Serial.print(n);
+    Serial.println();
+
+    for (i = 0; i < n; ++i) {
+      Serial.print(Serial.read(), DEC);
+      Serial.print(" ");
+    }
+
+    delay(500);
+    n = 0;
   }
 
+  porta++;
+  porta %= NR_PORTAS;
   delay(10);
 }
 
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  set_serial(0);
-  Serial.println("Ligado\n");
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  set_serial(1);
-  Serial.println("Desligado\n");
-  delay(1000);
+  fun2();
 }
--- MultiplexadorSerial/src/main.c
+++ MultiplexadorSerial/src/main.c
@@ -5,6 +5,7 @@
 #include <util/delay.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <avr/interrupt.h>
 
 #define BUFFER_SIZE		32
 #define ADR_T1			C,0
@@ -18,12 +19,9 @@ enum Terminais
 	TERMINAL_4
 };
 
-void terminal_printf(enum Terminais terminal, const char str[], ...)
+void set_terminal(enum Terminais t)
 {
-	char buffer[BUFFER_SIZE];
-	va_list args;
-	
-	switch(terminal)
+	switch(t)
 	{
 		case TERMINAL_1:
 		RESET(ADR_T1);
@@ -45,7 +43,14 @@ void terminal_printf(enum Terminais terminal, const char str[], ...)
 		SET(ADR_T2);
 		break;
 	}
+}
+
+void terminal_printf(enum Terminais terminal, const char str[], ...)
+{
+	char buffer[BUFFER_SIZE];
+	va_list args;
 	
+	set_terminal(terminal);	
 	_delay_ms(1);
 	va_start(args, str);
 	vsnprintf(buffer, BUFFER_SIZE, str, args);
@@ -58,24 +63,70 @@ void terminal_printf(enum Terminais terminal, const char str[], ...)
 	return;
 }
 
-int main(void)
+void m_init(void)
 {
-	enum Terminais t;
-	
-	t = TERMINAL_1;
 	uart_init(UART_BAUD_SELECT(BAUD, F_CPU));
 	SET_OUTPUT(ADR_T1);
 	SET_OUTPUT(ADR_T2);
+	sei();
 	_delay_ms(500);
+}
+
+void fun1(void)
+{
+	enum Terminais terminal;
+	
+	terminal = TERMINAL_1;
+	m_init();
 	
 	while(1)
 	{
-		for (t = TERMINAL_1; t <= TERMINAL_4; ++t)
+		for (terminal = TERMINAL_1; terminal <= TERMINAL_4; ++terminal)
 		{
-			terminal_printf(t, "Hello Terminal %d\n\r", (t + 1));
+			terminal_printf(terminal, "Hello Terminal %d\n\r", (terminal + 1));
 		}
 		
 		_delay_ms(500);
+	}
+}
+
+void fun2(void)
+{
+	m_init();
+	RESET(ADR_T1);
+	RESET(ADR_T2);
+	uart_printf("Hello\n");
+	_delay_ms(1000);
+	
+	while(1);
+}
+
+int main(void)
+{
+	enum Terminais terminal;
+	uint16_t n, i;
+	
+	terminal = TERMINAL_1;
+	m_init();
+	
+	while(1)
+	{
+		for (terminal = TERMINAL_1; terminal <= TERMINAL_4; ++terminal)
+		{
+			if (uart_available())
+			{
+				_delay_ms(100);
+				n = uart_available();
+				uart_printf("Recebido %u no terminal %u\n", n, terminal);
+				
+				for (i = 0; i < n; ++i)
+				{
+					uart_printf("%d ", uart_getc());
+				}
+				
+				uart_printf("\n");
+			}
+		}
 	}
 	
 	return 0;
