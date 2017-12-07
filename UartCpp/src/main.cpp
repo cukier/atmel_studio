5ad6f5129aba0ddbe5550ac41fd5a9cdb2154c72
cuki2 <mauricio@krebs.com.br>
2017-12-07 11:32:29 -0200

    UartCpp Signed-off-by: cuki2 <mauricio@krebs.com.br>

--- UartCpp/src/main.cpp
+++ UartCpp/src/main.cpp
@@ -6,11 +6,20 @@
 
 using namespace serial;
 
+void clean(char *str, uint16_t s)
+{
+	while (--s)
+	{
+		*str++ = '\0';
+	}
+}
+
 int main(void)
 {
 	//uint8_t reg[8] = {'m', 'a', 'u', 'r', 'c', 'i', 'o'};
 	uint8_t cont;
 	double f_cont;
+	char str[40];
 	
 	cont = 0;
 	f_cont = 0;
@@ -20,9 +29,12 @@ int main(void)
 	while(1)
 	{
 		//uart.send(reg, 8);
-		uart << "Hello\n\r";
-		uart << cont++ << "\n\r";
-		uart << f_cont << "\n\r";
+		uart << "Int " << cont++ << "\n\r";
+		uart << "Float " << f_cont << "\n\r";
+		clean(str, 40);
+		uart >> str;
+		uart << "Disponivel: " << uart.available() << "\n\r";
+		uart << "Digitado :" << str << "\n\r";
 		f_cont += 0.1f;
 		_delay_ms(500);
 	}
