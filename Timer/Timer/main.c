a8ec33e6e6c0455a467a4ca1c9ce13f692e7d6a0
maurice <mauricio@krebs.com.br>
2018-04-30 13:07:39 -0300

    timers
    Signed-off-by: maurice <mauricio@krebs.com.br>

--- Timer/Timer/main.c
+++ Timer/Timer/main.c
@@ -5,46 +5,32 @@
 * Author : cuki
 */
 
+
+#define F_CPU 16000000ULL
+
+#include "timers.h"
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdbool.h>
 
-#define REG 0x00
-
-uint8_t ctrl;
-
-ISR(TIMER1_OVF_vect)
+void toogle_pin()
 {
-	ctrl = 1;
-	TCNT1 = REG;
-	PORTB ^= (1<<PB5);
+	PORTC ^= (1 << PORTC0);
 }
-
-void update(void)
-{
-	if (ctrl)
-	{
-		ctrl = 0;
-		PORTB ^= (1<<PB4);
-	}
-}
-
 
 int main(void)
 {
-	ctrl = 0;
+	DDRC |= (1 << DDC0);
 	
-	DDRB |= (1<<DDB5);
-	DDRB |= (1<<DDB4);
-	TIMSK1 |= (1<<TOIE1);
-	TCCR1B |= ((1<<CS11) | (1<<CS10));
-	TCCR1B |= (1<<CS11);
-	TCNT1 = REG;
+	timer_1_setCallBack(&toogle_pin);
+	timer_1_init(1000, F_CPU);
+	
 	sei();
 	
 	while (1)
 	{
-		update();
+		//update();
 	}
 	
 	return 0;
